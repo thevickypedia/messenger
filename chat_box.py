99059815c9080a56df3473332b5c804df1706e94
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-07-07 12:57:38 -0500

    implement messaging feature

--- chat_box.py
+++ chat_box.py
@@ -1,11 +1,22 @@
+from tkinter import *
 import socket
+import threading
+from tkinter import messagebox
 
 
 class ChatBox:
     user_socket = None
+    latest_msg = None
 
-    def __init__(self):
+    def __init__(self, master):
+        self.core = master
+        self.transcript_box = None
+        self.name_box = None
+        self.text_box = None
+        self.join_button = None
         self.socket_init()
+        self.chatbox_init()
+        self.message_listener()
 
     def socket_init(self):
         self.user_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,6 +24,87 @@ class ChatBox:
         user_port = 10000
         self.user_socket.connect((user_ip, user_port))
 
+    def chatbox_init(self):
+        self.core.title("Socket Chat")
+        self.core.resizable(0, 0)
+        self.chat_box()
+        self.display_name_section()
+        self.chat_area()
+
+    def message_listener(self):
+        thread = threading.Thread(target=self.msg_from_server, args=(self.user_socket,))
+        thread.start()
+
+    def msg_from_server(self, so):
+        while True:
+            size = so.recv(256)
+            if not size:
+                break
+            message = size.decode('utf-8')
+            if "joined" in message:
+                user = message.split(":")[1]
+                message = user + " has joined"
+                self.transcript_box.insert('end', message + '\n')
+                self.transcript_box.yview(END)
+            else:
+                self.transcript_box.insert('end', message + '\n')
+                self.transcript_box.yview(END)
+
+        so.close()
+
+    def display_name_section(self):
+        frame = Frame()
+        Label(frame, text='Enter your Name:', font=("Times", 16)).pack(side='left', padx=10)
+        self.name_box = Entry(frame, width=50, borderwidth=2)
+        self.name_box.pack(side='left', anchor='e')
+        self.join_button = Button(frame, text="Join", width=10, command=self.join_response).pack(side='left')
+        frame.pack(side='top', anchor='nw')
+
+    def chat_box(self):
+        frame = Frame()
+        Label(frame, text='Live Transcript', font=("Times", 12)).pack(side='top', anchor='w')
+        self.transcript_box = Text(frame, width=60, height=10, font=("Serif", 12))
+        scrollbar = Scrollbar(frame, command=self.transcript_box.yview, orient=VERTICAL)
+        self.transcript_box.config(yscrollcommand=scrollbar.set)
+        self.transcript_box.bind('<KeyPress>', lambda e: 'something')
+        self.transcript_box.pack(side='left', padx=10)
+        scrollbar.pack(side='right', fill='y')
+        frame.pack(side='top')
+
+    def chat_area(self):
+        frame = Frame()
+        Label(frame, text='Message:', font=("Times", 12)).pack(side='top', anchor='w')
+        self.text_box = Text(frame, width=60, height=3, font=("Serif", 12))
+        self.text_box.pack(side='left', pady=15)
+        self.text_box.bind()
+        frame.pack(side='top')
+
+    def join_response(self):
+        if len(self.name_box.get()) == 0:
+            messagebox.showerror(
+                "Enter your name!", "Enter your name to join messenger")
+            return
+        self.name_box.config(state='disabled')
+        self.user_socket.send(("joined:" + self.name_box.get()).encode('utf-8'))
+
+    def send_chat(self):
+        sender = self.name_box.get().strip() + ": "
+        data = self.text_box.get(1.0, 'end').strip()
+        message = (sender + data).encode('utf-8')
+        self.transcript_box.insert('end', message.decode('utf-8') + '\n')
+        self.transcript_box.yview(END)
+        self.user_socket.send(message)
+        self.text_box.delete(1.0, 'end')
+        return 'something'
+
+    def close_response(self):
+        if messagebox.askokcancel("Message from Chat Box", "Are you sue you want to leave?"):
+            self.core.destroy()
+            self.user_socket.close()
+
 
 if __name__ == '__main__':
-    ChatBox()
+    trigger = Tk()
+    chat_win = ChatBox(trigger)
+    trigger.protocol("WM_DELETE_WINDOW", chat_win.close_response)
+    trigger.mainloop()
--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 import socket
+import threading
 
 
 class ChatServer:
+    audience_list = []
+    latest_msg = ""
+
     def __init__(self):
         self.socket_fd = None
         self.listener()
@@ -14,6 +18,34 @@ class ChatServer:
         self.socket_fd.bind((server_ip, server_port))
         print("Listener activated. Awaiting connections..")
         self.socket_fd.listen(5)
+        self.threaded_message()
+
+    def receiver_list(self, client):
+        if client not in self.audience_list:
+            self.audience_list.append(client)
+
+    def receive_messages(self, so):
+        while True:
+            message_buff = so.recv(256)
+            if not message_buff:
+                break
+            self.latest_msg = message_buff.decode('utf-8')
+            self.show_to_audience(so)  # send to all clients
+        so.close()
+
+    def show_to_audience(self, senders_socket):
+        for client in self.audience_list:
+            so, (ip, port) = client
+            if so is not senders_socket:
+                so.sendall(self.latest_msg.encode('utf-8'))
+
+    def threaded_message(self):
+        while True:
+            client = so, (ip, port) = self.socket_fd.accept()
+            self.receiver_list(client)
+            print(f'Connection accepted from {ip}:{str(port)}')
+            thread = threading.Thread(target=self.receive_messages, args=(so,))
+            thread.start()
 
 
 if __name__ == "__main__":
